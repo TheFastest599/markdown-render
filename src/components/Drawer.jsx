b1074b4f91cfb4471e8c0342fff1bf6736ab165d
TheFastest599 <ribhusaha2003@gmail.com>
2025-09-22 09:53:41 +0530

    Fixes

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -60,40 +60,42 @@ function Drawer() {
                 <ul className="max-h-96 overflow-y-auto">
                   {Object.entries(contents).map(([id, item]) => (
                     <li key={id}>
-                      <Link
-                        href={`/${id}`}
+                      <div
                         className={`flex justify-between ${
                           id === pathname?.split('/')[1] ? 'menu-active' : ''
                         }`}
-                        onClick={() => {
-                          setSelectedId(id);
-                        }}
                       >
-                        <span className="truncate flex-1">{item.name}</span>
-                        <span className="flex-none">
-                          <button
-                            className="btn btn-square btn-ghost btn-xs"
-                            onClick={e => {
-                              e.stopPropagation();
-                              removeContent(id);
-                            }}
+                        <Link
+                          href={`/${id}`}
+                          className="truncate flex-1"
+                          onClick={() => {
+                            setSelectedId(id);
+                          }}
+                        >
+                          {item.name}
+                        </Link>
+                        <button
+                          className="btn btn-square btn-ghost btn-xs flex-none"
+                          onClick={e => {
+                            e.stopPropagation();
+                            removeContent(id);
+                          }}
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            className="inline-block h-4 w-4 stroke-current"
                           >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              className="inline-block h-4 w-4 stroke-current"
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M6 18L18 6M6 6l12 12"
-                              ></path>
-                            </svg>
-                          </button>
-                        </span>
-                      </Link>
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M6 18L18 6M6 6l12 12"
+                            ></path>
+                          </svg>
+                        </button>
+                      </div>
                     </li>
                   ))}
                 </ul>
